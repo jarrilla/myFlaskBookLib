689f7c3ecc934058589cc7e44895cff6ee00e29c
J. Arrillaga <jarrilla.services@gmail.com>
2020-05-10 21:00:38 -0400

    added DB fields to config & TestConfig class for unit tests

--- config.py
+++ config.py
@@ -1,6 +1,15 @@
 import os
 
+# project base directory.. used to define dev DB path
+baseDir = os.path.abspath(os.path.dirname(__file__))
+
+class TestConfig:
+  SQLALCHEMY_DATABASE_URI = 'sqlite://'
+  SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 class Config:
   SECRET_KEY = os.environ.get('SECRET_KEY') or \
     'someSecret12345'
-  
+  SQLALCHEMY_DATABASE_URI = os.environ.get('DB_URI') or \
+    'sqlite:///' + os.path.join(baseDir, 'app.db')
+  SQLALCHEMY_TRACK_MODIFICATIONS = False
