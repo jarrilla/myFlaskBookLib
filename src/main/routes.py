fe160f3f76538f5a7b20482baade138973279d38
J. Arrillaga <jarrilla.services@gmail.com>
2020-05-11 12:54:56 -0400

    added 'books' local to index route

--- src/main/routes.py
+++ src/main/routes.py
@@ -1,8 +1,10 @@
 from flask import render_template, url_for
 
 from src.main import bp
+from src.book.model import Book
 
 @bp.route('/')
 @bp.route('/index')
 def index():
-  return render_template('index.html', title='Home')
+  books = Book.query.all()
+  return render_template('index.html', title='Home', books=books)
