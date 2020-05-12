3330fa48216e6ecc8a97e2371f922e6e165ebed5
J. Arrillaga <jarrilla.services@gmail.com>
2020-05-12 12:22:37 -0400

    fixed indentation so app actually logs.. oops :)

--- src/__init__.py
+++ src/__init__.py
@@ -67,14 +67,14 @@ def create_app(config_class=Config):
 
   if not os.path.exists('logs'):
     os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/app.log', maxBytes=10240, backupCount=10)
-    file_handler.setFormatter(logging.Formatter(
-      '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
-    ))
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
+  file_handler = RotatingFileHandler('logs/app.log', maxBytes=10240, backupCount=10)
+  file_handler.setFormatter(logging.Formatter(
+    '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+  ))
+  file_handler.setLevel(logging.INFO)
+  app.logger.addHandler(file_handler)
 
-    app.logger.setLevel(logging.INFO)
-    app.logger.info('--- MyFlaskLib startup ---')
+  app.logger.setLevel(logging.INFO)
+  app.logger.info('--- MyFlaskLib startup ---')
 
   return app
