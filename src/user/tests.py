76ef1172518f3696b8e905d09b43bf7340df1f69
J. Arrillaga <jarrilla.services@gmail.com>
2020-05-11 20:55:59 -0400

    added test cases for book entries

--- src/user/tests.py
+++ src/user/tests.py
@@ -2,7 +2,7 @@ import unittest
 
 from config import TestConfig
 from src import create_app, db
-from src.user.models import User
+from src.models import User
 
 class UserCases(unittest.TestCase):
   def setUp(self):
@@ -16,15 +16,29 @@ class UserCases(unittest.TestCase):
     db.drop_all()
     self.app_context.pop()
 
-  def test_user_creation(self):
+  def test_check_password(self):
     u = User(username='user', email='user@user.com')
     u.set_password('password')
     db.session.add(u)
     db.session.commit()
 
-    self.assertTrue(u.id != None)
-    self.assertEqual(u.username, 'user')
-    self.assertEqual(u.email, 'user@user.com')
-    self.assertEqual(u.is_verified, False)
     self.assertFalse( u.check_password('whatever') )
     self.assertTrue( u.check_password('password') )
+
+  def test_entry_creation(self):
+    u = User(username='u', email='u@u.com')
+    db.session.add(u)
+    db.session.commit()
+
+    title = 'book one'
+    author = 'some author'
+
+    res1 = u.add_entry_to_collection(title=title, author=author)
+    res2 = u.add_entry_to_collection(title=title, author=author)
+    res3 = u.add_entry_to_collection(title='other title', author='other author')
+    db.session.commit()
+
+    # res1 = res2 b/c trying to insert same book shouldn't duplicate anything
+    # collection() should be equal to [ first_entry, second_entry ]
+    self.assertEqual(res1, res2)
+    self.assertEqual( u.collection().all(), [res1, res3] )
--- tests.py
+++ tests.py
@@ -2,8 +2,8 @@ import unittest
 from src.user.tests import UserCases
 from src.book.tests import BookCases
 
-UserCases()
-BookCases()
+#UserCases()
+#BookCases()
 
 if __name__ == '__main__':
   unittest.main(verbosity=2)
