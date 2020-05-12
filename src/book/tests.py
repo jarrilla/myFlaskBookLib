6e8a27b41845aaf8313b2f41c03d20971ed71116
J. Arrillaga <jarrilla.services@gmail.com>
2020-05-11 20:55:36 -0400

    removed obsolete tests

--- src/book/tests.py
+++ src/book/tests.py
@@ -3,7 +3,7 @@ import unittest
 
 from config import TestConfig
 from src import create_app, db
-from src.book.models import Book, UserBookEntry
+from src.models import Book, UserBookEntry
 
 class BookCases(unittest.TestCase):
   def setUp(self):
@@ -17,38 +17,3 @@ class BookCases(unittest.TestCase):
     db.drop_all()
     self.app_context.pop()
 
-  def test_book_creation(self):
-    b = Book(title='book one', author='some author')
-    db.session.add(b)
-    db.session.commit()
-
-    self.assertEqual(b.title, 'book one')
-    self.assertEqual(b.author, 'some author')
-    self.assertTrue(b.id != None)
-
-# Make some books and some UserBookEntries, check that book entries correctly point to books
-  def test_new_book_entry_refs(self):
-    b1 = Book(title='book one', author='some author')
-    b2 = Book(title='book two', author='some author')
-    b3 = Book(title='book three', author='some author')
-    db.session.add(b1)
-    db.session.add(b2)
-    db.session.add(b3)
-    db.session.commit()
-
-    today = datetime.now().date()
-
-    e1 = UserBookEntry( book_id=b1.id)
-    e2 = UserBookEntry( book_id=b1.id)
-    e3 = UserBookEntry( book_id=b2.id)
-    e4 = UserBookEntry( book_id=b3.id)
-    db.session.add(e1)
-    db.session.add(e2)
-    db.session.add(e3)
-    db.session.add(e4)
-    db.session.commit()
-
-    self.assertEqual(e1.book, b1)
-    self.assertEqual(e2.book, b1)
-    self.assertEqual(e3.book, b2)
-    self.assertEqual(e4.book, b3)
