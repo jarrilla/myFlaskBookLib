bcff279758a35ea9c08774dc34f27739c77476e1
J. Arrillaga <jarrilla.services@gmail.com>
2020-05-12 12:19:53 -0400

    added basic 500 handling and logging

--- src/__init__.py
+++ src/__init__.py
@@ -4,6 +4,9 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import logging
+import os
 
 # mailer
 mail = Mail()
@@ -24,8 +27,6 @@ def create_app(config_class=Config):
   mail.init_app(app)
 
   # Create DB resources
-  from src.models import Book, User, UserBookEntry
-
   db.init_app(app)
   Migrate(app, db)
 
@@ -43,5 +44,37 @@ def create_app(config_class=Config):
   app.register_blueprint(main_bp)
   app.register_blueprint(user_bp)
   app.register_blueprint(book_bp)
+
+
+  # Set up logging
+  if not app.debug:
+    if app.config['MAIL_SERVER']:
+      auth = None
+      if app.config['MAIL_USERNAME'] and app.config['MAIL_PASSWORD']:
+        auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+      secure = None
+
+      if app.config['MAIL_USE_TLS']:
+        secure = ()
+      mail_handler = SMTPHandler(
+        mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+        fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+        toaddrs=app.config['ADMINS'], subject='MyFlaskLib Exception',
+        credentials=auth, secure=secure
+      )
+      mail_handler.setLevel(logging.ERROR)
+      app.logger.addHandler(mail_handler)
+
+  if not os.path.exists('logs'):
+    os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/app.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+      '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+    ))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('--- MyFlaskLib startup ---')
 
   return app
